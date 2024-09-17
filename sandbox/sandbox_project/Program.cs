951f2239bd6280bd2204dc4c0f3dd8df1e2bc125
Abdul Mahl <mdu_abdul@icloud.com>
2024-09-17 21:09:51 +0200

    Finished with the analyze / performance hw.

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -4,10 +4,20 @@ public class Program
 {
     static void Main(string[] args)
     {
-        // This project is here for you to use as a "Sandbox" to play around
-        // with any code or ideas you have that do not directly apply to
-        // one of your projects.
+        var numbers = new List<int>();
+        numbers.Add(55);
+        numbers.Add(58);
+        numbers.Add(45);
+        numbers.Remove(1);
+        numbers.Insert(1, 88);
 
-        Console.WriteLine("Hello Sandbox World!");
+        int count = numbers.Count;
+        foreach (int number in numbers)
+        {
+            string price = string.Format("{0:C}", number);
+            Console.WriteLine($"Dynamic Array Value: {price}");
+        }
+
+        Console.WriteLine($"Dynamic Array Count is: {count}");
     }
 }
